c559d30d127b639f2cff872e43cdd756b97e6a60
Marcio Mendes <mmendes.as@gmail.com>
2020-02-22 22:59:49 -0300

    feat: setup password hash with bcryptjs

--- fastfeet-backend/.eslintrc.js
+++ fastfeet-backend/.eslintrc.js
@@ -1,23 +1,23 @@
 module.exports = {
   env: {
     browser: true,
-    es6: true
+    es6: true,
   },
   extends: ['airbnb-base', 'prettier'],
   plugins: ['prettier'],
   globals: {
     Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly'
+    SharedArrayBuffer: 'readonly',
   },
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: 'module'
+    sourceType: 'module',
   },
   rules: {
     'prettier/prettier': 'error',
     'class-methods-use-this': 'off',
-    'no-params-reassign': 'off',
+    'no-param-reassign': 'off',
     camelcase: 'off',
-    'no-unused-vars': ['error', { argsIgnorePattern: 'next' }]
-  }
+    'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
+  },
 };
