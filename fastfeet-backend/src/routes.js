d98617d27d3786c00bc464c1b12d9d38ce3ea25f
Marcio Mendes <mmendes.as@gmail.com>
2020-02-22 21:44:56 -0300

    chore: enable es6 with sucrase and add nodemon

--- fastfeet-backend/src/routes.js
+++ fastfeet-backend/src/routes.js
@@ -1,9 +1,9 @@
-const { Router } = require('express');
+import { Router } from 'express';
 
 const routes = new Router();
 
 routes.get('/', (req, res) => {
-  return res.json({ message: 'first commit' });
+  return res.json({ message: 'enable es6' });
 });
 
-module.exports = routes;
+export default routes;
