a6c6a382739a67ccce7d2e7ba2429179b938870f
Marcio Mendes <mmendes.as@gmail.com>
2020-02-23 10:43:20 -0300

    feat: setup multer for file upload

--- fastfeet-backend/src/routes.js
+++ fastfeet-backend/src/routes.js
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
@@ -7,6 +9,7 @@ import RecipientController from './app/controllers/RecipientController';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 // public routes
 routes.post('/users', UserController.store);
@@ -16,5 +19,9 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 routes.post('/recipients', RecipientController.store);
+
+routes.post('/files', upload.single('file'), (req, res) => {
+  return res.json({ ok: true });
+});
 
 export default routes;
