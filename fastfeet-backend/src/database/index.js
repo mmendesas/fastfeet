e3b481105ceffcfdb2ab93447a18d3d004ad8c75
Marcio Mendes <mmendes.as@gmail.com>
2020-02-23 11:40:44 -0300

    feat: save file info in DB

--- fastfeet-backend/src/database/index.js
+++ fastfeet-backend/src/database/index.js
@@ -4,8 +4,9 @@ import databaseConfig from '../config/database';
 
 import User from '../app/models/User';
 import Recipient from '../app/models/Recipient';
+import File from '../app/models/File';
 
-const models = [User, Recipient];
+const models = [User, Recipient, File];
 
 class Database {
   constructor() {
--- fastfeet-backend/src/routes.js
+++ fastfeet-backend/src/routes.js
@@ -5,6 +5,7 @@ import multerConfig from './config/multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
+import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -20,8 +21,6 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 routes.post('/recipients', RecipientController.store);
 
-routes.post('/files', upload.single('file'), (req, res) => {
-  return res.json({ ok: true });
-});
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
