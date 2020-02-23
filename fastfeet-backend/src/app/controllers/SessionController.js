ec4507e969651174f9ef5036f3dbaeb2b2c0d96b
Marcio Mendes <mmendes.as@gmail.com>
2020-02-23 09:33:01 -0300

    feat: add data validation

--- fastfeet-backend/src/app/controllers/SessionController.js
+++ fastfeet-backend/src/app/controllers/SessionController.js
@@ -1,10 +1,23 @@
 import jwt from 'jsonwebtoken';
+import * as Yup from 'yup';
 
 import User from '../models/User';
 import authConfig from '../../config/auth';
 
 class SessionController {
   async store(req, res) {
+    // validate paylooad against schema
+    const schema = Yup.object({
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'session validation fails' });
+    }
+
     const { email, password } = req.body;
 
     // check if user exists
--- fastfeet-backend/src/app/controllers/UserController.js
+++ fastfeet-backend/src/app/controllers/UserController.js
@@ -1,7 +1,24 @@
+import * as Yup from 'yup';
+
 import User from '../models/User';
 
 class UserController {
   async store(req, res) {
+    // validate paylooad against schema
+    const schema = Yup.object({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string()
+        .required()
+        .min(6),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'validation fails' });
+    }
+
     const { email } = req.body;
 
     // check if user already exists
@@ -17,6 +34,25 @@ class UserController {
   }
 
   async update(req, res) {
+    // validate paylooad against schema
+    const schema = Yup.object({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string()
+        .min(6)
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ),
+      confirmPassword: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      ),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'validation fails' });
+    }
+
     const { email, oldPassword } = req.body;
 
     const user = await User.findByPk(req.userId);
