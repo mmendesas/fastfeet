0e96dbd9b70e5e1fc7adc659ed0e0d7ab9e7df2d
Marcio Mendes <mmendes.as@gmail.com>
2020-02-22 22:04:18 -0300

    chore: setting up eslint + prettier + editorconfig

--- fastfeet-backend/src/routes.js
+++ fastfeet-backend/src/routes.js
@@ -2,8 +2,6 @@ import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'enable es6' });
-});
+routes.get('/', (req, res) => res.json({ message: 'enable es6' }));
 
 export default routes;
