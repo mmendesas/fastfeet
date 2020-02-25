6182aa26d8b254d9c75a4300093521e7c42c0107
Marcio Mendes <mmendes.as@gmail.com>
2020-02-25 12:41:35 -0300

    feat: setting up store with redux-saga

--- fastfeet-web/src/App.js
+++ fastfeet-web/src/App.js
@@ -2,17 +2,22 @@ import React from 'react';
 import { Router } from 'react-router-dom';
 
 import './config/ReactotronConfig';
+import { Provider } from 'react-redux';
 import GlobalStyle from './styles/global';
 
 import Routes from './routes';
 import history from './services/history';
 
+import store from './store';
+
 function App() {
   return (
-    <Router history={history}>
-      <Routes />
-      <GlobalStyle />
-    </Router>
+    <Provider store={store}>
+      <Router history={history}>
+        <Routes />
+        <GlobalStyle />
+      </Router>
+    </Provider>
   );
 }
 
--- fastfeet-web/src/pages/Login/index.js
+++ fastfeet-web/src/pages/Login/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
 import { Form, Input } from '@rocketseat/unform';
 import * as Yup from 'yup';
