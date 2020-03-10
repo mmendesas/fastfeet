ec774f9c26ff3a65aea8ea72dbe745db7784db2f
Marcio Mendes <mmendes.as@gmail.com>
2020-03-09 22:44:11 -0300

    feat(back): enable cors in backend

--- fastfeet-backend/src/app.js
+++ fastfeet-backend/src/app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import { resolve } from 'path';
 import routes from './routes';
 
@@ -18,6 +19,7 @@ class App {
       '/files',
       express.static(resolve(__dirname, '..', 'tmp', 'uploads'))
     );
+    this.server.use(cors());
   }
 
   routes() {
