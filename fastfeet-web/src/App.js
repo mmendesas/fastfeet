f974270a0695423f54a575c6f282ec0f54b6c9f9
Marcio Mendes <mmendes.as@gmail.com>
2020-03-09 23:06:05 -0300

    feat(web): persist authed user

--- fastfeet-web/src/App.js
+++ fastfeet-web/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { PersistGate } from 'redux-persist/integration/react';
 import { Router } from 'react-router-dom';
 
 import './config/ReactotronConfig';
@@ -8,15 +9,17 @@ import GlobalStyle from './styles/global';
 import Routes from './routes';
 import history from './services/history';
 
-import store from './store';
+import { store, persistor } from './store';
 
 function App() {
   return (
     <Provider store={store}>
-      <Router history={history}>
-        <Routes />
-        <GlobalStyle />
-      </Router>
+      <PersistGate persistor={persistor}>
+        <Router history={history}>
+          <Routes />
+          <GlobalStyle />
+        </Router>
+      </PersistGate>
     </Provider>
   );
 }
--- fastfeet-web/src/routes/Route.js
+++ fastfeet-web/src/routes/Route.js
@@ -6,7 +6,7 @@ import { Route, Redirect } from 'react-router-dom';
 import DefaultLayout from '../pages/_layouts/default';
 import AuthLayout from '../pages/_layouts/auth';
 
-import store from '../store';
+import { store } from '../store';
 
 export default function RouteWrapper({
   component: Component,
