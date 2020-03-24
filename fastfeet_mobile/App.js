5f764b575c90e835ed1e72877ef1f9b39fc35970
Marcio Mendes <mmendes.as@gmail.com>
2020-03-24 00:16:21 -0300

    chore(mob): init project structure

---
+++ fastfeet_mobile/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet, View, Text, StatusBar} from 'react-native';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <View style={styles.body}>
+          <View style={styles.sectionContainer}>
+            <Text style={styles.sectionTitle}>Hello world</Text>
+          </View>
+        </View>
+      </SafeAreaView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  body: {
+    backgroundColor: Colors.white,
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+});
+
+export default App;
