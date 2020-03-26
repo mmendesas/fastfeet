27e8f7e3e32c6c22b0ac540fd2300e344f97546f
Marcio Mendes <mmendes.as@gmail.com>
2020-03-25 23:07:48 -0300

    feat(mob): setup reactotron

--- fastfeet_mobile/src/index.js
+++ fastfeet_mobile/src/index.js
@@ -2,6 +2,8 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { StatusBar } from 'react-native';
 
+import './config/ReactotronConfig';
+
 import Routes from './routes';
 
 export default function App() {
