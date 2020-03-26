57384c46adad5f30238f2f0af9a41d98a241e06a
Marcio Mendes <mmendes.as@gmail.com>
2020-03-26 01:13:42 -0300

    feat(mob): add unform to input

--- fastfeet_mobile/src/components/Input/index.js
+++ fastfeet_mobile/src/components/Input/index.js
@@ -1,20 +1,35 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-underscore-dangle */
 /* eslint-disable react/jsx-props-no-spreading */
-import React, { forwardRef } from 'react';
+import React, { useRef, useEffect } from 'react';
 import { oneOfType, object, array, string } from 'prop-types';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import { useField } from '@unform/core';
 
 import { Container, TInput } from './styles';
 
-function Input({ style, icon, ...props }, ref) {
+function Input({ name, style, icon, ...props }) {
+  const inputRef = useRef(null);
+  const { fieldName, registerField, defaultValue = '' } = useField(name);
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputRef.current,
+      path: '_lastNativeText',
+    });
+  }, [fieldName, registerField]);
+
   return (
     <Container style={style}>
       {icon && <Icon name={icon} size={20} color="#333" />}
-      <TInput {...props} ref={ref} />
+      <TInput ref={inputRef} defaultValue={defaultValue} {...props} />
     </Container>
   );
 }
 
 Input.propTypes = {
+  name: string.isRequired,
   icon: string,
   style: oneOfType([object, array]),
 };
@@ -24,4 +39,4 @@ Input.defaultProps = {
   style: {},
 };
 
-export default forwardRef(Input);
+export default Input;
